75f4f07a4359461034bd5d1f1d809e25d9dd0276
Rohit Singh <rohit00710@outlook.com>
2025-01-18 21:25:20 +0530

    rust python

---
+++ fib.py
@@ -0,0 +1,7 @@
+
+import rust_python_demo
+
+
+
+print(rust_python_demo.fib(10))
+print(rust_python_demo.sum_as_string(7,9))
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+
+#[pyfunction]
+fn fib(n : u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+
+    fib(n-1) + fib(n-2)
+
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn rust_python_demo(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(fib, m)?)?;
+    Ok(())
+}
